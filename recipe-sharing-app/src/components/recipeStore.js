f272279745b012d5da7c27482025fa454a9ec8bb
Arafat Ali <aravatt22@gmail.com>
2025-03-04 12:37:04 +0300

    adding set recipe functionalityu

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,50 @@
+// import create from 'zustand';
+
+// const useRecipeStore = create(set => ({
+//     recipes: [],
+//     searchTerm: '',
+//     filteredRecipes: [],
+//     // setRecipes
+
+//     // Action to add a recipe
+//     addRecipe: (newRecipe) => set(state => ({
+//         recipes: [...state.recipes, newRecipe],
+//     })),
+
+//     // Action to update a recipe
+//     updateRecipe: (updatedRecipe) => set(state => ({
+//         recipes: state.recipes.map(recipe =>
+//             recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+//         ),
+//     })),
+
+//     // Action to delete a recipe
+//     deleteRecipe: (recipeId) => set(state => ({
+//         recipes: state.recipes.filter(recipe => recipe.id !== recipeId),
+//     })),
+
+//     // Action to set the search term
+//     setSearchTerm: (term) => set({ searchTerm: term }),
+
+//     // Action to filter recipes based on the search term
+//     filterRecipes: () => set(state => ({
+//         filteredRecipes: state.recipes.filter(recipe =>
+//             recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()) ||
+//             recipe.ingredients.some(ingredient => ingredient.toLowerCase().includes(state.searchTerm.toLowerCase()))
+//         ),
+//     })),
+// }));
+
+// export default useRecipeStore;
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
     recipes: [],
     searchTerm: '',
     filteredRecipes: [],
-    // setRecipes
+
+    // Action to set the recipes (used to initialize or replace the recipes)
+    setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 
     // Action to add a recipe
     addRecipe: (newRecipe) => set(state => ({
