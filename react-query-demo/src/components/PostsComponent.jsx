9b1527e54e51e2245f6f58d8c505a37bbb7328d5
Arafat Ali <aravatt22@gmail.com>
2025-03-09 21:57:39 +0300

    adding error mesage

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -9,10 +9,10 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-    const { data, isError, isLoading, refetch } = useQuery('posts', fetchPosts);
+    const { data, isError, isLoading, error, refetch } = useQuery('posts', fetchPosts);
 
     if (isLoading) return <p>Loading...</p>;
-    if (isError) return <p>Error fetching data</p>;
+    if (isError) return <p>Error: {error.message}</p>; {/* Show the error message */ }
 
     return (
         <div>
