f20943e847921540bf03ea380b032997d74d99d4
Arafat Ali <aravatt22@gmail.com>
2025-03-09 22:27:03 +0300

    adding routing

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -12,6 +12,22 @@ const Profile = () => {
                     <li><Link to="settings">Profile Settings</Link></li>
                 </ul>
             </nav>
+            <Router>
+                <Routes>
+                    {/* Home Route */}
+                    <Route path="/" element={<Home />} />
+
+                    {/* Protected route for profile */}
+                    <Route path="/profile" element={<ProtectedRoute />}>
+                        {/* Nested routes under /profile */}
+                        <Route path="details" element={<ProfileDetails />} />
+                        <Route path="settings" element={<ProfileSettings />} />
+                    </Route>
+
+                    {/* Dynamic route for blog post */}
+                    <Route path="/post/:postId" element={<BlogPost />} />
+                </Routes>
+            </Router>
             <Outlet /> {/* Render nested routes like ProfileDetails and ProfileSettings */}
         </div>
     );
