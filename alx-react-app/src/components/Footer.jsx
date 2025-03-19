5fee49affd609c1ef3791b400c8136749f23505b
Arafat Ali <aravatt22@gmail.com>
2025-03-19 14:35:55 +0300

    update trhe footer, header , main contenct

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,9 +1,9 @@
-function Footer(){
-    return(
+const Footer = () => {
+    return (
+        <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+            <p>&copy; 2025 My Favorite Cities</p>
+        </footer>
+    );
+};
 
-     <footer >
-        <p>© 2023 City Lovers</p>
-</footer >
-    )
-}
 export default Footer;
